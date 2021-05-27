4d7dbe915e4bf03b7d7869ba8f013b0296c5964e
Marc Angelo Chiapco <66046436+inn0ichi@users.noreply.github.com>
2021-05-27 08:11:54 +0800

    breed

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import * as React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import Breed from "./src/screens/Breed"
+import Profile from "./src/screens/Profile"
+
 const Drawer = createDrawerNavigator();
 
 export default function App() {
@@ -10,6 +12,7 @@ export default function App() {
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="Home" component={Breed} />
+        <Drawer.Screen name="Profile" component={Profile} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
